7f3b82e185997bc5d06aeec5b633b77c88fb66a4
Sagar <scp1406@gmail.com>
2024-03-04 17:54:07 -0500

    gui: defined edit button

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import PySimpleGUI as sg
 label = sg.Text("Enter a to-do item")
 input_box = sg.InputText(tooltip="Enter to-do", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=fn.get_todos(), key='todos',
+                      enable_events=True, size=(45, 10))
+edit_button = sg.Button("Edit")
 
 window = sg.Window(title="My To-do App",
                    layout=[[label], [input_box, add_button]],
